04169427fd2a1a03efea56d8777553d35a60ede6
Suabsh Dhami <110603740+gitsubash44@users.noreply.github.com>
2024-07-21 22:16:12 +0545

    work in onPage

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -13,8 +13,43 @@ class _OnboardingState extends State<Onboarding> {
     return Scaffold(
       backgroundColor: Color(0xffecefe8),
       body: Container(
+        margin: EdgeInsets.only(top: 50.0),
         child: Column(
-          children: [],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+          Image.asset('images/headphone.PNG'),
+            Padding(
+              padding: EdgeInsets.only(left: 20.0),
+              child:  Text(
+                "Explore\nThe best\nProduct",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(right: 20.0),
+                  padding: EdgeInsets.all(20.0),
+                  decoration: BoxDecoration(color: Colors.black, shape: BoxShape.circle),
+                  child:  Text(
+                    "Next",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
